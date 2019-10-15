a7444b0b86d931cc032804fb4b6484006cc0b33e
Daphné Aurouet <daphne.aurouet@gmail.com>
2019-10-15 08:14:50 +0200

    REVISED: sample size

--- Daphne/ElasticNet.py
+++ Daphne/ElasticNet.py
@@ -4,9 +4,21 @@ Created on Tue Oct  8 21:44:49 2019
 
 @author: daphne
 """
+########################################################################################################################
+##################                                  IMPORT MODULES                                    ##################
+########################################################################################################################
+import time
 import numpy as np
 import os
 import pandas as pd
+from sklearn import linear_model, metrics
+from sklearn.model_selection import GridSearchCV
+from itertools import cycle
+import matplotlib.pyplot as plt
+
+########################################################################################################################
+##################                                 LOADING THE DATA                                   ##################
+########################################################################################################################
 
 os.chdir(
     "C:/Users/daphn/Documents/Kaggle/house-prices-advanced-regression-techniques"
@@ -20,21 +32,140 @@ y = final["SalePrice"].copy()
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.8,
+                                                    test_size=0.1,
                                                     random_state=42)
 
-################### LASSO REGRESSION #####################################################
+########################################################################################################################
+##################                                 LASSO REGRESSION                                   ##################
+########################################################################################################################
 from sklearn.linear_model import Lasso
 
 regr_lasso = Lasso(fit_intercept=False)
 regr_lasso.fit(X_train, y_train)
 
 print(regr_lasso.coef_)
+print(regr_lasso.alpha) # why 1 ?!
 
 pred_lasso = regr_lasso.predict(X_test)
-
 RMSE = np.sqrt(np.mean(pred_lasso - y_test) ** 2)
 R2_lasso = regr_lasso.score(X_test, y_test)
+# 0.84
+
+########################################### Hyper parameters tuning ####################################################
+lasso_params = {'alpha': range(1, 10000, 10)}
+
+Lasso_hyper = GridSearchCV(linear_model.Lasso(fit_intercept=False, precompute=True),
+                           param_grid=lasso_params)
+Lasso_fit = Lasso_hyper.fit(X_train, y_train)
+
+Lasso_fit.best_estimator_
+# alpha = 1161
+
+Lasso_pred = Lasso_fit.predict(X_test)
+R2_Lasso_hyper = metrics.r2_score(Lasso_pred, y_test)
+# less good that the normal lasso...
+
+################################################## CROSS VALIDATION ####################################################
+# This is to avoid division by zero while doing np.log10
+EPSILON = 1e-4
+
+# LassoLarsIC: least angle regression with BIC/AIC criterion
+from sklearn.linear_model import LassoLarsIC
+model_bic = LassoLarsIC(criterion='bic')
+t1 = time.time()
+bic_fit = model_bic.fit(X, y)
+t_bic = time.time() - t1
+alpha_bic_ = model_bic.alpha_
+bic_pred = bic_fit.predict(X_test)
+R2_bic = metrics.r2_score(bic_pred, y_test)
+# 0,556 du coup pas bon
+
+from sklearn.linear_model import LassoLarsIC
+model_aic = LassoLarsIC(criterion='aic')
+aic_fit = model_aic.fit(X, y)
+alpha_aic_ = model_aic.alpha_
+aic_pred = aic_fit.predict(X_test)
+R2_aic = metrics.r2_score(aic_pred, y_test)
+# 0,756 du coup pas top
+
+def plot_ic_criterion(model, name, color):
+    alpha_ = model.alpha_ + EPSILON
+    alphas_ = model.alphas_ + EPSILON
+    criterion_ = model.criterion_
+    plt.plot(-np.log10(alphas_), criterion_, '--', color=color,
+             linewidth=3, label='%s criterion' % name)
+    plt.axvline(-np.log10(alpha_), color=color, linewidth=3,
+                label='alpha: %s estimate' % name)
+    plt.xlabel('-log(alpha)')
+    plt.ylabel('criterion')
+
+plt.figure()
+plot_ic_criterion(model_aic, 'AIC', 'b')
+plot_ic_criterion(model_bic, 'BIC', 'r')
+plt.legend()
+plt.title('Information-criterion for model selection (training time %.3fs)'
+          % t_bic)
+
+# #############################################################################
+# LassoCV: coordinate descent
+from sklearn.linear_model import LassoCV
+
+# Compute paths
+print("Computing regularization path using the coordinate descent lasso...")
+LassoCV_fit = LassoCV(cv=20).fit(X, y)
+LassoCV_pred = LassoCV_fit.predict(X_test)
+R2_LassoCV = metrics.r2_score(LassoCV_pred, y_test)
+# 0.60 pas top du tout
+
+# Display results
+m_log_alphas = -np.log10(model.alphas_ + EPSILON)
+
+plt.figure()
+ymin, ymax = 2300, 3800
+plt.plot(m_log_alphas, model.mse_path_, ':')
+plt.plot(m_log_alphas, model.mse_path_.mean(axis=-1), 'k',
+         label='Average across the folds', linewidth=2)
+plt.axvline(-np.log10(model.alpha_ + EPSILON), linestyle='--', color='k',
+            label='alpha: CV estimate')
+
+plt.legend()
+
+plt.xlabel('-log(alpha)')
+plt.ylabel('Mean square error')
+plt.title('Mean square error on each fold: coordinate descent '
+          '(train time: %.2fs)' % t_lasso_cv)
+plt.axis('tight')
+plt.ylim(ymin, ymax)
+
+# LassoLarsCV: least angle regression
+from sklearn.linear_model import LassoLarsCV
+# Compute paths
+print("Computing regularization path using the Lars lasso...")
+LassoLarsCV_fit = LassoLarsCV(cv=20).fit(X, y)
+LassoLarsCV_pred = LassoLarsCV_fit.predict(X_test)
+R2_LassoLarsCV = metrics.r2_score(LassoLarsCV_pred, y_test)
+# 0.776 du coup un peu meilleur
+
+# Display results
+m_log_alphas = -np.log10(model.cv_alphas_ + EPSILON)
+
+plt.figure()
+plt.plot(m_log_alphas, model.mse_path_, ':')
+plt.plot(m_log_alphas, model.mse_path_.mean(axis=-1), 'k',
+         label='Average across the folds', linewidth=2)
+plt.axvline(-np.log10(model.alpha_), linestyle='--', color='k',
+            label='alpha CV')
+plt.legend()
+
+plt.xlabel('-log(alpha)')
+plt.ylabel('Mean square error')
+plt.title('Mean square error on each fold: Lars (train time: %.2fs)'
+          % t_lasso_lars_cv)
+plt.axis('tight')
+plt.ylim(ymin, ymax)
+
+plt.show()
+################################################
 
 # cross-validation
 from sklearn.linear_model import LassoCV
@@ -69,14 +200,11 @@ dictR2lasso_cv
 # sklearn.linear_model.lasso_path and sklearn.linear_model.enet_path.
 
 ######################
-from itertools import cycle
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import lasso_path, enet_path
 
 # Compute paths
 eps = 5e-3  # the smaller it is the longer is the path
 
+from sklearn.linear_model import lasso_path
 print("Computing regularization path using the lasso...")
 alphas_lasso, coefs_lasso, _ = lasso_path(X_train, y_train, eps, fit_intercept=False)
 
@@ -85,6 +213,7 @@ alphas_positive_lasso, coefs_positive_lasso, _ = lasso_path(
     X_train, y_train, eps, positive=True, fit_intercept=False)
 
 print("Computing regularization path using the elastic net...")
+from sklearn.linear_model import enet_path
 alphas_enet, coefs_enet, _ = enet_path(
     X_train, y_train, eps=eps, l1_ratio=0.8, fit_intercept=False)
 
@@ -137,8 +266,6 @@ plt.axis('tight')
 plt.show()
 
 
-
-
 ### ElasticNet
 from sklearn.linear_model import ElasticNet
 
@@ -151,24 +278,25 @@ regr_En.alpha
 
 RMSE = np.sqrt(np.mean(pred_En - y_test) ** 2)
 R2_En = regr_En.score(X_test, y_test)
+# 0.837
 
 # CV
 from sklearn.linear_model import ElasticNetCV
-from sklearn.model_selection import GridSearchCV
+
 regr_En_cv = ElasticNetCV(cv = 3, fit_intercept = False,
                           normalize= False)
 
-parametersGrid = {"alpha": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+parametersGrid = {"alpha": range(0, 10000, 10),
                   "l1_ratio": np.arange(0.001, 1.0, 0.1)}
 
-grid = GridSearchCV(regr_En_cv, parametersGrid, scoring='r2', cv= 3
+grid = GridSearchCV(regr_En_cv, parametersGrid, scoring='r2', cv= 3)
 
 grid.fit(X_train, y_train)
 pred_En_cv = regr_En_cv.predict(X_test)
 R2_En_cv = regr_En_cv.score(X_test, y_test)
 regr_En_cv.alpha_
 
-dictR2EN_cv
+
 
 
 
@@ -181,8 +309,6 @@ plt.plot(regr_En.coef_,alpha=0.7, linestyle='none',marker='*',markersize=5,
 plt.ylabel('Coefficient Magnitude',fontsize=16)
 plt.legend(fontsize=13,loc=4)
 plt.show()
-
-
 
 
 
@@ -211,7 +337,7 @@ from sklearn import metrics
 
 models = {'OLS': linear_model.LinearRegression(),
          'Lasso': linear_model.Lasso(),
-         'Ridge': linear_model.Ridge(),
+         'Ridge': linear_model.Ridge()}
 
 X = list(final.drop(columns="SalePrice").columns)
 Y = ["SalePrice"]
@@ -219,8 +345,8 @@ Y = ["SalePrice"]
 test(models, final)
 
 
-lasso_params = {'alpha':[0.02, 0.024, 0.025, 0.026, 0.03]}
-ridge_params = {'alpha':[200, 230, 250,265, 270, 275, 290, 300, 500]}
+lasso_params = {'alpha': range(1, 10000, 10)}
+ridge_params = {'alpha': range(1, 10000, 10)}
 
 models2 = {'OLS': linear_model.LinearRegression(),
            'Lasso': GridSearchCV(linear_model.Lasso(),
@@ -229,13 +355,11 @@ models2 = {'OLS': linear_model.LinearRegression(),
                                param_grid=ridge_params).fit(final[X], final[Y]).best_estimator_,}
 
 test(models2, final)
+# 0.78 pour Ridge ! good good
 
 # Poly features added
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
-
-lasso_params = {'fit__alpha':[0.005, 0.02, 0.03, 0.05, 0.06]}
-ridge_params = {'fit__alpha':[550, 580, 600, 620, 650]}
 
 pipe1 = Pipeline([('poly', PolynomialFeatures()),
                  ('fit', linear_model.LinearRegression())])
@@ -243,6 +367,9 @@ pipe2 = Pipeline([('poly', PolynomialFeatures()),
                  ('fit', linear_model.Lasso())])
 pipe3 = Pipeline([('poly', PolynomialFeatures()),
                  ('fit', linear_model.Ridge())])
+
+lasso_params = {'alpha': range(2, 10000, 10)}
+ridge_params = {'alpha': range(2, 10000, 10)}
 
 models3 = {'OLS': pipe1,
            'Lasso': GridSearchCV(pipe2,
