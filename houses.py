6dccee83cf6dac26860ff3dbeb6bb11d540c6373
jguti21 <jordi_guti94@hotmail.com>
2019-10-24 16:53:06 +0200

    Add files via upload

--- houses.py
+++ houses.py
@@ -10,7 +10,22 @@ import seaborn
 import numpy as np
 import pandas as pd
 import os
-from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+
+
+def add_identity(axes, *line_args, **line_kwargs):
+    identity, = axes.plot([], [], *line_args, **line_kwargs)
+    def callback(axes):
+        low_x, high_x = axes.get_xlim()
+        low_y, high_y = axes.get_ylim()
+        low = max(low_x, low_y)
+        high = min(high_x, high_y)
+        identity.set_data([low, high], [low, high])
+    callback(axes)
+    axes.callbacks.connect('xlim_changed', callback)
+    axes.callbacks.connect('ylim_changed', callback)
+    return axes
+
 
 os.chdir('C:/Users/gutierj/Desktop/Programming/Kaggle/')
 
@@ -48,7 +63,7 @@ for x in train.columns:
 # Normalization of the floats
 from sklearn.preprocessing import StandardScaler
 
-train = train.fillna(0)
+train = train.fillna(train.mean())
 
 scaler = StandardScaler()
 norm = scaler.fit_transform(train[train.columns.intersection(list_float)])
@@ -174,186 +189,39 @@ for var in rest_var:
 #######################################
     
     
- 
-"""   
-#Select the X´s by removing Id and SalePrice
+####################################### 
+    # final cleaning
+#######################################    
+    #x_train = train[['OverallQual', 'GrLivArea']]
+#x_test = test[['OverallQual', 'GrLivArea']]
+
+#X's from test data used to predict
+x_test = test.loc[:, test.columns != 'Id']
+#x_test = np.log(x_test+1)
+
+#x_test = x_test.fillna(0)
+
+#Id for later's submission
+test_id = test.loc[:, test.columns == 'Id']
+
+#Target to predict in training
+y_train = train.loc[:, train.columns == 'SalePrice']
+y_train = np.ravel(y_train)
+
+#X's used to predict in training
 x_train = train.loc[:, train.columns != 'Id']
 x_train = x_train.loc[:, x_train.columns != 'SalePrice']
+#x_train = np.log(x_train+1)
+
+#x_train = x_train.fillna(0)
 
 
-
-#Select y train, SalePrice
-y_train = train.loc[:, train.columns == 'SalePrice']
-
-#pearsonr(train['LotArea'],train['SalePrice'])
-#pearsonr(train['OverallQual'],train['SalePrice']).sort()
-
-#sorted(train.corr(method='pearson')['SalePrice'])
-
-#split and prepare test data
-
-#these are the X's of the ID's we need to predict
-test_x = test.loc[:, test.columns != 'Id']
-
-#Id's for later's submission
-test_id = test.loc[:, test.columns == 'Id']
-
-# Initialize linear regression object
-from sklearn import linear_model
-
-regr  = linear_model.LinearRegression()
-
-
-# Train the model
-
-
-
-regr.fit(x_train, y_train)
-
-#numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-
-#newdf = x_train.select_dtypes(include=numerics)
-
-
-
-#regr.fit(newdf, y_train)
-
-print ('Coefficients: \n', regr.coef_)
-
-#plt.scatter(x_train, y_train, color='black')
-
-#Ridge Regression
-from sklearn.linear_model import Ridge
-rreg = Ridge(alpha = 0.001)
-rreg.fit(x_train, y_train)
-
-print ('Coefficients: \n', rreg.coef_)
-
-#Lasso
-from sklearn.linear_model import Lasso
-
-lreg = Lasso(alpha = 0.1)
-
-lreg.fit(x_train, y_train)
-
-print ('Coefficients: \n', lreg.coef_)  #many coefs set to 0
-
-
-#convert test_x to dummies
-test_xdummy = pd.get_dummies(test_x)
-
-#fill NA's
-test_xdummy.fillna(test_xdummy.mean(), inplace=True)
-
-
-pred = lreg.predict(test_xdummy)
-
-
-########### 
-#Run a simple regression and prepare predictions
-########### 
-
-
-x_pred = train[['OverallQual', 'GrLivArea']]
-test_pred = test_x[['OverallQual', 'GrLivArea']]
-
-#Initializes OLS
-ols  = linear_model.LinearRegression()
-
-#Fits ols with selected variables from training and training target
-ols.fit(x_pred, y_train)
-
-#predicts, with OLS model, using the X's received for the test
-pred = ols.predict(test_pred)
-
-
-# paste Id and prediction for submission
-test_id = test.loc[:, test.columns == 'Id']
-test_id.loc[:,1] = pred
-
-submission = test_id
-
-submission.columns = ["Id", "SalePrice"]
-
-
-submission.to_csv(r'Submission.csv', index = False)
-
-"""
 
 #################################
 #Gradient Boosting regression
 #################################
 
-"""
-from sklearn.ensemble import GradientBoostingRegressor
 
-#X's from test data used to predict
-x_test = test.loc[:, test.columns != 'Id']
-
-#Id for later's submission
-test_id = test.loc[:, test.columns == 'Id']
-
-#Target to predict in training
-y_train = train.loc[:, train.columns == 'SalePrice']
-
-
-#Given values: n_estimators=500, max_depth = 4, learning_rate = 0.01, loss="ls"
-clf = GradientBoostingRegressor(n_estimators=600, max_depth = 5, learning_rate = 0.01, loss='ls', verbose=1)
-clf = GradientBoostingRegressor(n_estimators=700, max_depth = 6, learning_rate = 0.1, loss='ls', verbose=1)
-
-#X's used to predict in training
-x_train = train.loc[:, train.columns != 'Id']
-x_train = x_train.loc[:, x_train.columns != 'SalePrice']
-
-clf.fit(x_train, y_train)
-
-#predicts, with model, using the X's received for the test
-pred = clf.predict(x_test)
-
-
-# paste Id and prediction for submission
-
-#add prediction to second column of test_id
-test_id.loc[:,1] = pred
-
-#rename
-submission = test_id
-submission.columns = ["Id", "SalePrice"]
-
-
-submission.to_csv(r'Submission.csv', index = False)
-"""
-
-##########################
-# parameter tuning
-##########################
-
-
-
-##################
-# data preparation
-##################
-
-#x_train = train[['OverallQual', 'GrLivArea']]
-#x_test = test[['OverallQual', 'GrLivArea']]
-
-#X's from test data used to predict
-x_test = test.loc[:, test.columns != 'Id']
-
-#Id for later's submission
-test_id = test.loc[:, test.columns == 'Id']
-
-#Target to predict in training
-y_train = train.loc[:, train.columns == 'SalePrice']
-
-#X's used to predict in training
-x_train = train.loc[:, train.columns != 'Id']
-x_train = x_train.loc[:, x_train.columns != 'SalePrice']
-
-
-###############
-# modelling
-###############
 
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
@@ -364,17 +232,30 @@ from sklearn.metrics import make_scorer
 mean_squared_error = make_scorer(mean_squared_error)
 #rms = sqrt(mean_squared_error(y_actual, y_predicted))
 
+
+"""
+#Given values: n_estimators=500, max_depth = 4, learning_rate = 0.01, loss="ls"
+clf = GradientBoostingRegressor(n_estimators=600, max_depth = 5, learning_rate = 0.01, loss='ls', verbose=1)
+clf = GradientBoostingRegressor(n_estimators=700, max_depth = 6, learning_rate = 0.1, loss='ls', verbose=1)
+
+#X's used to predict in training
+x_train = train.loc[:, train.columns != 'Id']
+x_train = x_train.loc[:, x_train.columns != 'SalePrice']
+
+clf.fit(x_train, y_train)
+"""
+
 def model_gradient_boosting_tree(Xtrain,Xtest,ytrain):
     
     X_train = Xtrain
     y_train = ytrain 
     gbr = GradientBoostingRegressor(random_state=0, loss='ls')
     param_grid = {
-        'n_estimators': [500,600,700,800],
+        'n_estimators': [420, 450, 470],
        # 'max_features': [20,15],
-	    'max_depth': [3,4,5,6],
-        'learning_rate': [0.01, 0.1],
-       'subsample': [1, 0.7, 0.8]
+	    'max_depth': [0.3, 0.5, 0.6],
+        'learning_rate': [0.009, 0.01, 0.015],
+       'subsample': [0.95, 0.9, 0.85]
     }
     model = GridSearchCV(estimator=gbr, param_grid=param_grid, n_jobs=1, cv=10, scoring=mean_squared_error)
     model.fit(X_train, y_train)
@@ -387,15 +268,33 @@ def model_gradient_boosting_tree(Xtrain,Xtest,ytrain):
     y_pred = model.predict(Xtest)
     return y_pred, model.best_score_
 
-pred = model_gradient_boosting_tree(x_train,x_test,y_train)
+#pred = model_gradient_boosting_tree(x_train,x_test,y_train)
+
+clf = GradientBoostingRegressor(n_estimators=470, max_depth = 0.3, learning_rate = 0.015, loss='ls', subsample = 0.85)
+
+clf = clf.fit(x_train, y_train)
+
+pred = clf.predict(x_test)
+
+"""
+f, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlim((0, max([max(pred[0]), max(y_train)]) + 10000))
+ax.set_ylim((0, max([max(pred[0]), max(y_train)]) + 10000))
+ax.set_xlabel("Actual")
+ax.set_ylabel("Predicted")
+ax.scatter(y_train, pred[0], c=".3")
+add_identity(ax, color='r', ls='--')
+plt.show()
+"""
 
 #Best CV Score:
-#1994522269.359976
+#6315501866.2019005
 
 #add prediction to second column of test_id
 test_id = test.loc[:, test.columns == 'Id']
-test_id.loc[:,1] = pred[0]
+test_id.loc[:,1] = predx
 
+#test_id.loc[:,1] = preds[0]
 #rename
 submission = test_id
 submission.columns = ["Id", "SalePrice"]
@@ -403,6 +302,93 @@ submission.columns = ["Id", "SalePrice"]
 
 submission.to_csv(r'Submission.csv', index = False)
 
+#################################
+# XGB
+#################################
+"""
+SET HTTPS_PROXY=https://ap-python-proxy:x2o7rCPYuN1JuV8H@p-gw.ecb.de:9090
+
+pip install --trusted-host pypi.org xgboost
+"""
+
+
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=123)
+
+
+xg_reg = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.3, learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 10)
+
+xg_reg.fit(x_train,y_train)
+
+
+preds = xg_reg.predict(x_test)
+
+#########
+
+
+params = {"objective":"reg:linear",'colsample_bytree': 0.3,'learning_rate': 0.1,
+                'max_depth': 5, 'alpha': 10}
+
+
+data_dmatrix = xgb.DMatrix(data=x_train,label=y_train)
+
+cv_results = xgb.cv(dtrain=data_dmatrix, params=params, nfold=3,
+                    num_boost_round=50,early_stopping_rounds=10,metrics="rmse", as_pandas=True, seed=123)
+
+print(cv_results, verbose = True)
+
+predx = 0.5*pred + 0.5*preds
+
+
+#########################
+# optimize XGB
+########################
+
+from scipy import stats
+from sklearn.model_selection import RandomizedSearchCV, KFold
+
+xgb = xgb.XGBRegressor(objective ='reg:linear')
+param_dist = {'n_estimators': stats.randint(150, 500),
+              'learning_rate': stats.uniform(0.01, 0.07),
+              'subsample': stats.uniform(0.3, 0.7),
+              'max_depth': [3, 4, 5, 6, 7, 8, 9],
+              'colsample_bytree': stats.uniform(0.5, 0.45),
+              'min_child_weight': [1, 2, 3]
+             }
+clf = RandomizedSearchCV(xgb, param_distributions = param_dist, n_iter = 25, scoring = 'f1', error_score = 0, verbose = 3, n_jobs = -1)
+
+numFolds = 5
+folds = KFold(n_splits = numFolds, shuffle = True)
+
+estimators = []
+results = np.zeros(len(x_train))
+score = 0.0
+for train_index, test_index in folds.split(x_train):
+    X_train, X_test = x_train.iloc[train_index,:], x_train.iloc[test_index,:]
+    Y_train, Y_test = y_train.iloc[train_index].values.ravel(), y_train.iloc[test_index].values.ravel()
+    clf.fit(X_train, Y_train)
+
+    estimators.append(clf.best_estimator_)
+    results[test_index] = clf.predict(X_test)
+    score += f1_score(y_test, results[test_index])
+score /= numFolds
+
+#################################
+# Random forest
+#################################
+
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+random_forest = RandomForestClassifier(max_depth=100, n_estimators=20)
+random_forest.fit(x_train, y_train)
+
+scores_rf = cross_val_score(random_forest, x_train, y_train, cv=10)
+scores_rf
 
 """
 
@@ -486,17 +472,6 @@ scores_nv = cross_val_score(naive_class, X_clean, target, cv=10)
 scores_nv
 
 
-#Gradient Boosting regression
-import numpy as np
-
-import matplotlib.pyplot as plt
-from sklearn.ensemble import GradientBoostingRegressor
-
-
-#Given values: n_estimators=500, max_depth = 4, learning_rate = 0.01, loss="ls"
-clf = GradientBoostingRegressor(n_estimators=600, max_depth = 5, learning_rate = 0.01, loss='ls', verbose=1)
-
-clf.fit(x_train, y_train )
 
 import sklearn
 fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_train, clf.predict(x_train), pos_label=1)
