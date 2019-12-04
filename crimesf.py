121bbe678f993d0a6a762f200beb4f0bf8839754
jguti21 <jordi_guti94@hotmail.com>
2019-12-04 15:23:30 +0100

    Add files via upload

--- crimesf.py
+++ crimesf.py
@@ -103,19 +103,16 @@ test.replace({'X': -120.5, 'Y': 90.0}, np.NaN, inplace=True)
 
 from sklearn.preprocessing import LabelEncoder
 le2 = LabelEncoder()
-y = le2.fit_transform(train.pop('Category'))
 
-x = le2.fit_transform(train.pop('PdDistrict'))
+x = le2.fit_transform(train.loc[:, train.columns == 'PdDistrict'])
 
 x = x.reshape(-1,1)
 
+x_test = le2.fit_transform(test.loc[:, test.columns =='PdDistrict'])
+x_test = x_test.reshape(-1,1)
 
-#https://chrisalbon.com/machine_learning/naive_bayes/multinomial_logistic_regression/
+y = le2.fit_transform(train.loc[:, train.columns == 'Category'])
 
-from sklearn.linear_model import LogisticRegression
-
-# Create one-vs-rest logistic regression object
-clf = LogisticRegression(random_state=0, multi_class='multinomial', solver='newton-cg')
 
 
 # Train model
@@ -129,13 +126,136 @@ model = gnb.fit(x,y)
 
 
 #predict
-x_test = le2.fit_transform(test.pop('PdDistrict'))
-x_test = x_test.reshape(-1,1)
 
 pred = gnb.predict(x_test)
 
 ############## Create submission
+#y = le2.fit_transform(train.pop('Category'))
+#pred = le2.inverse_transform(pred)
 
+pred_string = le2.inverse_transform(pred)
+pred_string = pred_string.flatten('K')
+
+test_id = test.loc[:, test.columns == 'Id']
+test_id.append(pd.series(pred_string))
+
+a = pd.DataFrame(test_id, pred_string[:])
+
+
+pd.wide_to_long(a, i = 'Id', j = "value", stubnames = "fdjks")
+
+submission = pd.DataFrame(pred, columns=le2.inverse_transform
+                              (np.linspace(0, 38, 39, dtype='int16')),
+                              index=test.pop('Id'))
+
+
+##############################################################################
+##############################################################################
+
+import os
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+
+train = pd.read_csv("train.csv")
+
+y = train["Category"].unique()
+
+test = pd.read_csv("test.csv")
+
+data_dict = {}
+count = 1
+for data in y:
+    data_dict[data] = count
+    count+=1
+train["Category"] = train["Category"].replace(data_dict)
+
+#Replacing the day of weeks
+data_week_dict = {
+    "Monday": 1,
+    "Tuesday":2,
+    "Wednesday":3,
+    "Thursday":4,
+    "Friday":5,
+    "Saturday":6,
+    "Sunday":7
+}
+train["DayOfWeek"] = train["DayOfWeek"].replace(data_week_dict)
+test["DayOfWeek"] = test["DayOfWeek"].replace(data_week_dict)
+
+#District
+district = train["PdDistrict"].unique()
+data_dict_district = {}
+count = 1
+for data in district:
+    data_dict_district[data] = count
+    count+=1 
+train["PdDistrict"] = train["PdDistrict"].replace(data_dict_district)
+test["PdDistrict"] = test["PdDistrict"].replace(data_dict_district)
+
+
+train = train.drop(['Resolution'], axis = 1)
+
+
+#knn on numeric columns
+
+features = ["DayOfWeek", "PdDistrict",  "X", "Y"]
+X_train = train[features]
+y_train = train["Category"]
+X_test = test[features]
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+
+from collections import OrderedDict
+data_dict_new = OrderedDict(sorted(data_dict.items()))
+
+
+result_dataframe = pd.DataFrame({
+    "Id": test["Id"]
+})
+    
+    
+for key,value in data_dict_new.items():
+    result_dataframe[key] = 0
+
+count = 0
+for item in predictions:
+    for key,value in data_dict.items():
+        if(value == item):
+            result_dataframe[key][count] = 1
+    count+=1
+result_dataframe.to_csv("submission_knn.csv", index=False)
+
+
+####### Logistic
+
+from sklearn.linear_model import LogisticRegression
+lgr = LogisticRegression()
+lgr.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+
+#print(type(predictions))
+result_dataframe = pd.DataFrame({
+    "Id": test["Id"]
+})
+for key,value in data_dict_new.items():
+    result_dataframe[key] = 0
+count = 0
+for item in predictions:
+    for key,value in data_dict.items():
+        if(value == item):
+            result_dataframe[key][count] = 1
+    count+=1
+result_dataframe.to_csv("submission_logistic.csv", index=False) 
+
+##############################################################################
+
+
+##############################################################################
 
 #add prediction to second column of test_id
 test_id = test.loc[:, test.columns == 'Id']
@@ -148,6 +268,13 @@ submission.columns = ["Id", "SalePrice"]
 
 
 
+
+#https://chrisalbon.com/machine_learning/naive_bayes/multinomial_logistic_regression/
+
+from sklearn.linear_model import LogisticRegression
+
+# Create one-vs-rest logistic regression object
+clf = LogisticRegression(random_state=0, multi_class='multinomial', solver='newton-cg')
 
 
 import lightgbm as lgb
